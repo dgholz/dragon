4d6e50d732592d78370ecbe9801939111ee4b16b
Daniel George Holz <dgholz@gmail.com>
2014-08-17 01:52:38 -0700

    switching binary of the 2.4.1 exercises

---
+++ 2.4.1/main.cc
@@ -0,0 +1,28 @@
+#include "Parser.h"
+#include "a.h"
+#include "b.h"
+#include "c.h"
+#include <iostream>
+#include <memory>
+
+int main( int argc, char* *argv ) {
+    std::unique_ptr<Parser> p;
+
+    if(argc>1) {
+        Parser::char_iter cin(std::cin); // blocks...
+        switch(*argv[1]) {
+            case 'a': p = std::unique_ptr<Parser>(new a(cin)); break;
+            default: std::cerr << "choose a question a,b, or c" << std::endl; return 1;
+        }
+    }
+    else {
+        std::cerr << "choose a question a,b, or c" << std::endl;
+        return 1;
+    }
+
+    p->start();
+    if(p->lookahead != Parser::char_iter()) {
+        std::cerr << "Trailing input: " << *p->lookahead << std::endl;
+        return 1;
+    }
+}

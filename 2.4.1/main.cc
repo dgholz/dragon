936d9ddf96c45a54af13378f8911beab3da084df
Daniel George Holz <dgholz@gmail.com>
2014-08-20 01:09:24 -0700

    template function to dispatch to chosen grammar

--- 2.4.1/main.cc
+++ 2.4.1/main.cc
@@ -4,14 +4,19 @@
 #include "c.h"
 #include <iostream>
 
+template<typename T> void parse( icharstream ics ) {
+    T grammar(ics);
+    grammar();
+}
+
 int main( int argc, char* *argv ) {
     icharstream ics(std::cin);
 
     if(argc>1) {
         switch(*argv[1]) {
-            case 'a': a(ics).start(); break;
-            case 'b': b(ics).start(); break;
-            case 'c': c(ics).start(); break;
+            case 'a': parse<a>( ics ); break;
+            case 'b': parse<b>( ics ); break;
+            case 'c': parse<c>( ics ); break;
             default: std::cerr << "choose a question a,b, or c" << std::endl; return 1;
         }
     }

1e35872fe385c7005fb0efb99b253bcce8d893ec
Daniel George Holz <dgholz@gmail.com>
2014-08-25 22:39:32 -0700

    tests for grammar for 2.4.1.b)

---
+++ 2.4.1/t/b.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+test_grammar_b() {
+  exec 9<&0 <<EOT
+()         0
+()()       0
+(          1
+)          1
+(())       0
+())()(     1
+(()())     0
+()()()()() 0
+)(         1
+EOT
+  while read input exit_value
+  do
+      echo "${input}" | egrep -v '^\s*(#|$)' >/dev/null || continue
+      echo "# ${input}"
+      echo "${input}" | exercise_2_4_1 b
+      assertEquals ${exit_value} $?
+  done
+  exec 0<&9
+}
+
+. shunit2

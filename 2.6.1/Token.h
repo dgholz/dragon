49578b439791b395f0d9b398821f0a11d1fc2454
Daniel George Holz <dgholz@gmail.com>
2014-09-05 23:12:42 -0700

    say no to inheiritance

--- 2.6.1/Token.h
+++ 2.6.1/Token.h
@@ -6,35 +6,9 @@
 
 namespace lexer {
     struct Token {
-        enum Tag { NUM, ID, TRUE, FALSE, PUNCT, ENDOFINPUT };
-        Token(Tag const& t) : tag(t) {};
-        Tag tag;
-        virtual std::ostream& ostr(std::ostream& os) const { os << "[ TOKEN ]"; return os; };
-    };
-
-    struct Num : Token {
-        Num(int const& v) : Token(Tag::NUM), value(v) {};
-        int const value;
-        virtual std::ostream& ostr(std::ostream& os) const { os << "[ NUM, " << value << " ]"; return os; };
-    };
-
-    struct Word : Token {
-        std::string const lexeme;
-        Word(Tag const& t, std::string const& s) : Token(t), lexeme(s) { };
-        virtual std::ostream& ostr(std::ostream& os) const { os << "[ WORD, " << lexeme << " ]"; return os; };
-    };
-
-    struct Punct : Token {
-        char const sym;
-        Punct(char const s) : Token(Tag::PUNCT), sym(s) { };
-        virtual std::ostream& ostr(std::ostream& os) const { os << "[ PUNCT, " << sym << " ]"; return os; };
-    };
-
-    struct EndOfInput : Token {
-        EndOfInput() : Token(Tag::ENDOFINPUT) {};
-        virtual std::ostream& ostr(std::ostream& os) const { os << "[ ENDOFINPUT ]"; return os; };
-    };
-
+        bool match(std::istreambuf_iterator);
+        std::vector<std::tuple>& add_payload(std::vector &v)
+    }
 }
 
 #endif

c5732380a52d08e332fcd6503525b8361a000c72
Daniel George Holz <dgholz@gmail.com>
2014-09-16 23:01:58 -0700

    use regex to match lexemes

--- 2.6.1/main.cc
+++ 2.6.1/main.cc
@@ -1,12 +1,28 @@
+#include <ostream>
+#include <vector>
+
+#include <boost/regex.hpp>
+
+#include "Tag.h"
 #include "Token.h"
-#include <iostream>
+#include "Lexeme.h"
+
+std::vector<Lexeme> lexemes() {
+    return std::vector<Lexeme> ({
+        Lexeme(
+            ".",
+            [](boost::cmatch const &m) {
+                return Token(Tag::HI, 1);
+            }
+        ),
+    });
+}
 
 int main( int argc, char* *argv ) {
-    lexer::Lexer l(std::cin);
-    while(!l.no_more_input()) {
-        lexer::Token const* t = &l.scan();
-        t->ostr(std::cout);
-        std::cout << ", ";
-    }
-    std::cout << std::endl;
+    auto l = lexemes();
+    auto m = l[0];
+    boost::cmatch g;
+    boost::regex_match("g", g, m.pattern);
+    Token t = m(g);
+    std::cout << t << std::endl;
 }

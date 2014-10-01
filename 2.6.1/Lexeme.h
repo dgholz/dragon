4ad450dbc44198cefd869e4e1523f1ec44b07a92
Daniel George Holz <dgholz@gmail.com>
2014-10-01 08:29:41 -0700

    remove cmatch from Lexeme

--- 2.6.1/Lexeme.h
+++ 2.6.1/Lexeme.h
@@ -11,11 +11,10 @@
 struct Lexeme {
     boost::regex const pattern;
     Tag const tag;
-    boost::cmatch cmatch;
 
     Lexeme(boost::regex const &re, Tag const &t) : pattern(re), tag(t) {};
     Lexeme(std::string const &res, Tag const &t) : pattern(boost::regex(res)), tag(t) {};
-    Token operator()() { return Token(tag, cmatch[0]); };
+    Token operator()(boost::cmatch const & cmatch) { return Token(tag, cmatch[0]); };
 };
 
 #endif

db48b411885950ef777d743551c93d86dd1a61ac
Daniel George Holz <dgholz@gmail.com>
2014-10-14 09:03:54 -0700

    make const first in type of params to Lexeme ctors etc.

--- 2.6.1/Lexeme.h
+++ 2.6.1/Lexeme.h
@@ -12,9 +12,9 @@ struct Lexeme {
     boost::regex const pattern;
     Tag const tag;
 
-    Lexeme(boost::regex const &re, Tag const &t) : pattern(re), tag(t) {};
-    Lexeme(std::string const &res, Tag const &t) : pattern(boost::regex(res)), tag(t) {};
-    Token operator()(boost::cmatch const & cmatch) { return Token(tag, cmatch[0]); };
+    Lexeme(const boost::regex &re, const Tag &t) : pattern(re), tag(t) {};
+    Lexeme(const std::string &res, const Tag &t) : pattern(boost::regex(res)), tag(t) {};
+    Token operator()(const boost::cmatch &cmatch) const { return Token(tag, cmatch[0]); };
 };
 
 #endif

1539966218ff0531231967bd503b26cf6b4406f6
Daniel George Holz <dgholz@gmail.com>
2014-10-14 09:04:32 -0700

    add equality op to Lexeme

--- 2.6.1/Lexeme.h
+++ 2.6.1/Lexeme.h
@@ -9,12 +9,18 @@
 #include "Token.h"
 
 struct Lexeme {
+
     boost::regex const pattern;
     Tag const tag;
 
     Lexeme(const boost::regex &re, const Tag &t) : pattern(re), tag(t) {};
     Lexeme(const std::string &res, const Tag &t) : pattern(boost::regex(res)), tag(t) {};
+
     Token operator()(const boost::cmatch &cmatch) const { return Token(tag, cmatch[0]); };
+    bool operator==(const Lexeme &l) const {
+        return pattern == l.pattern && tag == l.tag;
+    };
+
 };
 
 #endif

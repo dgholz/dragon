8e0ead5fb7ba26a42da6d1ec88873ada2dc09eac
Daniel George Holz <dgholz@gmail.com>
2014-10-24 09:13:18 -0700

    test for trailing newline

--- 2.6.1/t/lexer.sh
+++ 2.6.1/t/lexer.sh
@@ -8,6 +8,7 @@ test_input_empty() {
 test_lexer() {
   exec 9<&0 <<EOT
 2-1       0 [ NUM, 2 ], [ PUNCT, - ], [ NUM, 1 ], [ END_OF_INPUT ], 
+22-11\\\\n  0 [ NUM, 22 ], [ PUNCT, - ], [ NUM, 11 ], [ END_OF_INPUT, \\n ], 
 a         1 [ END_OF_INPUT, a ], 
 abc       1 [ END_OF_INPUT, a ], 
 EOT
@@ -15,7 +16,7 @@ EOT
   do
       echo "${input}" | egrep -v '^\s*(#|$)' >/dev/null || continue
       echo "# ${input}"
-      o=$(/bin/echo -n "${input}" | exercise_2_6_1)
+      o=$(/bin/echo -e -n "${input}" | exercise_2_6_1)
       assertEquals ${exit_value} $?
       echo ${o}
       assertEquals "${output}" "${o}"

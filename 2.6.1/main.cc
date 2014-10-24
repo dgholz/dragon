28ece6e88fbbc1db81b2f2c4776a8f76a1d46b11
Daniel George Holz <dgholz@gmail.com>
2014-10-24 09:19:27 -0700

    add template Tokenizer, templated on char type

--- 2.6.1/main.cc
+++ 2.6.1/main.cc
@@ -1,10 +1,9 @@
 #include <ostream>
-#include <array>
 #include <sstream>
 #include <unordered_map>
+#include <vector>
 
 #include <boost/regex.hpp>
-#include <boost/iterator/transform_iterator.hpp>
 
 #include "Tag.h"
 #include "Token.h"
@@ -22,58 +21,113 @@ std::vector<Lexeme<char>> const static lexemes({
     ),
 });
 
-typedef std::unordered_map<const Lexeme, boost::cmatch> mlex_match;
+template<typename T> struct Tokenizer {
+    typedef std::unordered_map<const Lexeme<T>, std::basic_string<T>> mlex_match;
+    typedef std::pair<const Lexeme<T>, std::basic_string<T>> pair_lex_match;
 
-template<typename T> mlex_match try_matching(T lex_itr, T const &lex_end, char const * const chars) {
-    mlex_match m;
-    while(lex_itr != lex_end) {
-        std::cout << lex_itr->tag << " match '" << chars << "'";
-        boost::cmatch cmatch;
-        if(boost::regex_match(chars, cmatch, lex_itr->pattern)) {
-            std::cout << ": " << cmatch[0];
-            m[*lex_itr] = cmatch;
-        }
-        std::cout << std::endl;
-        ++lex_itr;
+    Lookahead<T> p;
+    std::vector<Lexeme<T>> const lexemes;
+    std::basic_stringstream<T> buf;
+    mlex_match best_matches;
+
+    Tokenizer() : p(), lexemes(), buf(), best_matches() {};
+    Tokenizer(Lookahead<T> &_p, std::vector<Lexeme<T>> const &_lexemes): p(_p), lexemes(_lexemes), buf(), best_matches() {};
+
+    bool operator==(Tokenizer<T> const  &t) const {
+        return p.no_more_input() == t.p.no_more_input() && buf.str() == t.buf.str();
     }
-    return m;
-}
 
-static const Lexeme l_get_first(const std::pair<const Lexeme, boost::cmatch> &p) {
-    return p.first;
-}
+    bool operator!=(Tokenizer<T> const &t) const {
+        return !(this->operator==(t));
+    }
 
-static bool lm_cmp(const std::pair<const Lexeme, boost::cmatch> &a, const std::pair<const Lexeme, boost::cmatch> &b) {
-    return a.second.length() > b.second.length();
-}
+    bool add_one_char() {
+        if(!p.no_more_input()) {
+            buf << *p;
+            p++;
+            return true;
+        }
+        return false;
+    };
+
+    void cleanup_match(const std::basic_string<T> &matching_text) {
+        best_matches = mlex_match();
+        buf.str(buf.str().erase(0, matching_text.length()));
+        buf.seekp(0, std::ios_base::end);
+    };
+
+    bool all_partial_match(mlex_match const &ml, std::string::size_type const &l) {
+        return !ml.empty() && std::all_of(ml.begin(), ml.end(), [&l](pair_lex_match const &plm) { return plm.second.length() != l; });
+    }
+
+    bool try_matching() {
+        bool new_matches = false;
+        boost::match_flag_type flags = boost::match_continuous | boost::match_partial;
+        boost::match_results<const T*> match;
+        for(auto lex : lexemes) {
+            if(boost::regex_search(buf.str().c_str(), match, lex.pattern, flags)) {
+                best_matches[lex] = std::basic_string<T>(match[0]);
+                new_matches = true;
+            }
+        }
+        return new_matches;
+    }
+
+    Token<T> next() {
+        if(p.no_more_input()){
+            if(buf.str().empty()) {
+                return Token<T>(Tag::END_OF_INPUT);
+            }
+        }
+        while(!all_partial_match(best_matches, buf.str().length()) && add_one_char()) {
+            if(!try_matching()) {
+                auto t = Token<T>(Tag::END_OF_INPUT, buf.str());
+                cleanup_match(buf.str());
+                return t;
+                // throw NoToken(buf.str());
+            }
+        }
+
+        if(best_matches.empty()) {
+            // no match?
+            auto t = Token<T>(Tag::END_OF_INPUT, buf.str());
+            cleanup_match(buf.str());
+            return t;
+            // throw NoToken(buf.str());
+        }
+
+        // find longest lexeme which matches some of buf
+
+        auto lm_cmp = [](const pair_lex_match &a, const pair_lex_match &b) {
+            return a.second.length() > b.second.length();
+        };
+        auto longest = std::max_element(best_matches.begin(), best_matches.end(), lm_cmp)->second.length();
+
+        for(auto lex: lexemes) {
+            auto lm_itr = best_matches.find(lex);
+            if(lm_itr != best_matches.end()) {
+                if(lm_itr->second.length() == longest) {
+                    auto token = lm_itr->first( lm_itr->second );
+                    cleanup_match(lm_itr->second);
+                    return token;
+                }
+            }
+        }
+        return Token<T>(Tag::END_OF_INPUT);
+    }
+};
+
 
 int main(int argc, char* *argv) {
-    auto p = Lookahead(std::cin);
-    std::stringstream buf;
+    auto p = Lookahead<char>(std::cin);
+    Tokenizer<char> t(p, lexemes);
+    Tokenizer<char> tend;
 
-    while(!p.no_more_input()) {
-        buf << *p; p++;
+    while(t != tend) {
+        std::cout << t.next() << ", ";
 
-        mlex_match what_m = try_matching(lexemes.begin(), lexemes.end(), buf.str().c_str());
-        mlex_match prev_wm;
-        while(!what_m.empty()) {
-            buf << *p; p++;
-            what_m.insert(prev_wm.begin(), prev_wm.end());
-            prev_wm = what_m;
-            auto lex_itr = boost::make_transform_iterator(what_m.begin(), l_get_first);
-            auto lex_end = boost::make_transform_iterator(what_m.end(), l_get_first);
-            what_m = try_matching(lex_itr, lex_end, buf.str().c_str());
-        }
-
-        auto longest = std::max_element(prev_wm.begin(), prev_wm.end(), lm_cmp);
-        if(longest == prev_wm.end()) {
-            std::cout << "could not match token for '" << buf.str() << "'" << std::endl;
-            return 1;
-        }
-        auto m = longest->first( longest->second );
-        std::cout << "best match was " << m << std::endl;
-        buf.str( buf.str().substr( longest->second.length() - 1 ) );
     }
+    std::cout << std::endl;
 
     return 0;
 }

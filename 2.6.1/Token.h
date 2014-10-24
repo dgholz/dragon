35715653c974f4cc9f22c63db24debb4a2fc12d4
Daniel George Holz <dgholz@gmail.com>
2014-10-24 09:04:49 -0700

    escape newlines when printing Token payloads

--- 2.6.1/Token.h
+++ 2.6.1/Token.h
@@ -4,6 +4,8 @@
 #include <ostream>
 
 #include "boost/variant.hpp"
+#include <boost/format.hpp>
+#include <boost/algorithm/string.hpp>
 
 #include "Tag.h"
 
@@ -17,10 +19,37 @@ template<typename T> struct Token {
     Token(Tag const t) : tag(t) {};
 };
 
+template<typename T> struct quote_unprintables {
+    template<typename FindResultT>
+    std::basic_string<T> operator()(const FindResultT& unprint_itr) const
+    {
+        std::basic_stringstream<T> buf;
+        std::unordered_map<T, std::basic_string<T>> unprint_map {
+            { '\n', "\\n" }
+        };
+
+        for (auto i: unprint_itr) {
+            auto up_itr = unprint_map.find(i);
+            if ( up_itr != unprint_map.end() ) {
+                buf << up_itr->second;
+            }
+        }
+        return buf.str();
+    }
+};
+
 template<typename T> std::ostream& operator<<(std::ostream &os, Token<T> const &t) {
     os << "[ " << t.tag;
     for(auto p : t.payload) {
-        os << ", " << p;
+        os << ", ";
+        if(p.type() == typeid(std::basic_string<T>)) {
+            std::basic_string<T> s(boost::get<std::basic_string<T>>(p));
+            boost::find_format_all(s, boost::token_finder(!boost::is_print()), quote_unprintables<T>());
+            os << s;
+        }
+        else {
+            os << p;
+        }
     }
     os << " ]";
     return os;

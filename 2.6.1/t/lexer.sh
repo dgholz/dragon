89a9331a9098831b156da12d0ff817c92e741ff9
Daniel George Holz <dgholz@gmail.com>
2014-10-27 08:44:28 -0700

    test case for input with newlines

--- 2.6.1/t/lexer.sh
+++ 2.6.1/t/lexer.sh
@@ -8,7 +8,6 @@ test_input_empty() {
 test_lexer() {
   exec 9<&0 <<EOT
 2-1       0 [ NUM, 2 ], [ PUNCT, - ], [ NUM, 1 ], [ END_OF_INPUT ], 
-22-11\\\\n  0 [ NUM, 22 ], [ PUNCT, - ], [ NUM, 11 ], [ END_OF_INPUT, \\n ], 
 a         1 [ END_OF_INPUT, a ], 
 abc       1 [ END_OF_INPUT, a ], 
 EOT
@@ -16,9 +15,25 @@ EOT
   do
       echo "${input}" | egrep -v '^\s*(#|$)' >/dev/null || continue
       echo "# ${input}"
-      o=$(/bin/echo -e -n "${input}" | exercise_2_6_1)
+      o=$(/bin/echo -n "${input}" | exercise_2_6_1)
       assertEquals ${exit_value} $?
-      echo ${o}
+      /bin/echo ${o}
+      assertEquals "${output}" "${o}"
+  done
+  exec 0<&9
+}
+
+test_lexer_with_newline() {
+  exec 9<&0 <<EOT
+22-11       0 [ NUM, 22 ], [ PUNCT, - ], [ NUM, 11 ], [ END_OF_INPUT, \\\\n ], 
+EOT
+  while read input exit_value output
+  do
+      echo "${input}" | egrep -v '^\s*(#|$)' >/dev/null || continue
+      echo "# ${input}"
+      o=$(echo "${input}" | exercise_2_6_1)
+      assertEquals ${exit_value} $?
+      /bin/echo "${o}"
       assertEquals "${output}" "${o}"
   done
   exec 0<&9
